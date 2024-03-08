136a90addc3c40ef5be4cdddc8278e771d6c8c3e
Wuhanqingg <greatvegetable@hotmail.com>
2024-03-08 17:33:30 +0800

    add a file

--- leetcode2834.cpp
+++ leetcode2834.cpp
@@ -1,3 +1,11 @@
+/*给你两个正整数：n 和 target 。
+
+如果数组 nums 满足下述条件，则称其为 美丽数组 。
+
+nums.length == n.
+nums 由两两互不相同的正整数组成。
+在范围 [0, n-1] 内，不存在 两个 不同 下标 i 和 j ，使得 nums[i] + nums[j] == target 。
+返回符合条件的美丽数组所可能具备的 最小 和，并对结果进行取模 109 + 7。*/
 class Solution {
 public:
     int minimumPossibleSum(int n, int target) {
